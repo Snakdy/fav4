ee148f6c8e02947a32ae961c6bc2f1b8c0fb7dc3
Django Cass <django@dcas.dev>
2021-09-12 13:03:19 +1000

    Added simple request logging

--- cmd/fav4/main.go
+++ cmd/fav4/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
 	"gitlab.com/autokubeops/serverless"
 	"net/http"
 )
 
 func main() {
 	serverless.Run(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Infof("%s %s %s %s", r.Method, r.URL.Path, r.UserAgent(), r.RemoteAddr)
 		_, _ = w.Write([]byte("Hello " + r.UserAgent()))
 	}))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module gitlab.dcas.dev/open-source/fav4
 
 go 1.17
 
-require gitlab.com/autokubeops/serverless v0.1.0
+require (
+	github.com/sirupsen/logrus v1.8.1
+	gitlab.com/autokubeops/serverless v0.1.0
+)
+
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
