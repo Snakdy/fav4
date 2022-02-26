0b04b5613b793f1fa3e5a3b542d0a353a1dc7849
Django Cass <django@dcas.dev>
2022-02-26 11:09:39 +1100

    Enabled CORS Updated dependencies

--- cmd/fav4/main.go
+++ cmd/fav4/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/djcass44/go-tracer/tracer"
+	"github.com/rs/cors"
 	"gitlab.com/autokubeops/serverless"
 	"gitlab.dcas.dev/open-source/fav4/internal/api"
 )
 
 func main() {
 	route := api.NewIconAPI()
-	serverless.NewBuilder(tracer.NewHandler(route)).
+	serverless.NewBuilder(tracer.NewHandler(cors.AllowAll().Handler(route))).
 		WithPrometheus().
 		Run()
 }
